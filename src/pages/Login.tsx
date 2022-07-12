9407a64b1f0b9764b9de63b6009ecff80b15c9f6
Egorlaptop <egorlavrinovich@gmail.com>
2022-07-12 22:29:33 +0300

    added form and input

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,16 +3,16 @@ import '../SCSS/style.scss'
 import {useState} from "react";
 import Input from "../components/UI/Input/Input";
 import {IUser} from "../types/types";
+import { useForm, SubmitHandler } from "react-hook-form";
 const Login = () => {
         const [data,setdata] = useState<boolean>(false)
         const [user,setuser] = useState<IUser>({name:'',email:'',id:Date.now(),password:''})
         function SendAllData():IUser|null{
-                console.log(user)
                return user
         }
     return (
         <div className='registration__wrapper'>
-            <div className='body'>
+            <form className='body'>
                     {(data)?<h2>Регистрация</h2>:<h2>Вход</h2>}
                     {data&&<div className='login'>
                             <Input value={user.name} type='text' placeholder='Введите имя' onChange={(e)=>setuser({...user,name:(e.target as HTMLInputElement).value})}/>
@@ -26,16 +26,16 @@ const Login = () => {
                     {data&&<div className='pass'>
                             <Input type='password' placeholder='Повторите пароль'/>
                     </div>}
-                    {(data)?<div onClick={SendAllData} className='enter'>Регистрация</div>:<div className='enter'>Войти</div>}
-                    {(!data)?<div className='remind'>
+                    {(data)?<button type='submit' onClick={SendAllData} className='enter'>Регистрация</button>:<button className='enter'>Войти</button>}
+                    {(!data) ? <div className='remind'>
                             <div className='forgotpass'>Забыли пароль?</div>
                             <div onClick={() => setdata(!data)} className='registration'>Регистрация</div>
-                    </div>:
+                        </div> :
                         <div className='remind'>
-                                <div onClick={() => setdata(!data)} className='registration'>Уже есть аккаунт?</div>
+                            <div onClick={() => setdata(!data)} className='registration'>Уже есть аккаунт?</div>
                         </div>
                     }
-            </div>
+            </form>
         </div>
     );
 };
