59aebffb184327f8497cdf5e085f357ef844315b
Egorlaptop <egorlavrinovich@gmail.com>
2022-07-17 19:30:55 +0300

    header-adaptiv

--- src/components/common/Header/header.tsx
+++ src/components/common/Header/header.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Input from "../../UI/Input/Input";
 import {UseChangeInput} from '../../../hooks/UseChangeInput'
+import {HeartFill,Basket3Fill,PersonFill,Search} from 'react-bootstrap-icons'
+
 const Header = () => {
     const search = UseChangeInput()
     return (
         <div className='header-container'>
             <div className="logo"><img src='https://www.zootovar-spb.ru/assets/img/logo.png'/></div>
-            <div className="search"><Input value={search.str} onChange={search.ChangeState} type='text' placeholder='Я ищу...'/></div>
+            <div className="search">
+                <div className='poisk'>
+                <Input value={search.str} onChange={search.ChangeState} type='text' placeholder='Я ищу...'/>
+                <div className='lupa'><Search/></div>
+                </div>
+            </div>
             <div className="entrance">
-                <div className="Enter">Вход</div>
-                <div className="favotite">Избранное</div>
-                <div className="basket">Корзина</div>
+                <div className="Enter"><i><PersonFill size={24}/></i><div className='vhod'>Вход</div></div>
+                <div className="favotite"><i><HeartFill size={19}/></i><div className='fav'>Избранное</div></div>
+                <div className="basket"><i><Basket3Fill size={22}/></i><div className='bask'>Корзина</div></div>
             </div>
         </div>
     );
