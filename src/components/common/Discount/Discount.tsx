91557cc7a6aaf44203c8560d5cd680c52859e0ef
Egor <egorlavrinovich@gmail.com>
2022-07-20 16:38:58 +0300

    Add functional ADVERSTINGBLOCK

--- src/components/common/Discount/Discount.tsx
+++ src/components/common/Discount/Discount.tsx
@@ -5,16 +5,30 @@ import images from './Allimports'
 import {ArrowLeft,ArrowRight} from 'react-bootstrap-icons'
 const Discount = React.memo(() => {
     const [activeblock,setactiveblock] = useState(false)
-    const [activeimage,setactiveimage] = useState(6)
+    const [activeimage,setactiveimage] = useState(1)
     const Available = useAppSelector((state)=>state.categories.choosedcategory)
     useEffect(()=>{if(Available==='discount')setactiveblock(true)
     else setactiveblock(false)},[Available])
+    function SpinBlock(perem:string){
+        switch (perem){
+            case 'right':setactiveimage((x)=>x+1); break
+            case 'left':setactiveimage((x)=>x-1);break
+        }
+    }
+    setTimeout(()=>{
+        setactiveimage((x)=>x+1)
+    },6000)
+    useEffect(()=>{
+        if(activeimage>6) setactiveimage(0)
+        if(activeimage<0) setactiveimage(6)
+
+    },[activeimage])
     return (
         <div className='discount-wrapper'>
             <div className='discount-block'>
-                <div className='left-but'><ArrowLeft/></div>
+                <div onClick={()=>SpinBlock('left')} className='left-but'><ArrowLeft/></div>
                 <div className='top-image'><img src={images[activeimage]}/></div>
-                <div className='right-but'><ArrowRight/></div>
+                <div onClick={()=>SpinBlock('right')} className='right-but'><ArrowRight/></div>
             </div>
             <div className='discount-goods'></div>
         </div>
