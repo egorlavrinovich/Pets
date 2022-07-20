a6d8176a9340a936f41092bfdab671d947176603
Egor <egorlavrinovich@gmail.com>
2022-07-20 10:21:13 +0300

    Add Functional

--- src/components/common/AnimalFilterBar/FilterBar.tsx
+++ src/components/common/AnimalFilterBar/FilterBar.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 // @ts-ignore
-import cat from '../../../assets/ico/cat.ico'
+import cats from '../../../assets/ico/cat.ico'
 // @ts-ignore
-import dog from '../../../assets/ico/dog.ico'
+import dogs from '../../../assets/ico/dog.ico'
 // @ts-ignore
 import fish from '../../../assets/ico/fish.ico'
 // @ts-ignore
 import bird from '../../../assets/ico/bird.ico'
 // @ts-ignore
-import rat from '../../../assets/ico/rat.ico'
-
+import rats from '../../../assets/ico/rat.ico'
+import classNames from "classnames";
 const FilterBar = () => {
+    const [active,setactive] = useState('')
+    const Categories = [{
+        type:'cats',
+        name:"Кошки",
+        url:cats
+    },
+        {
+            type:'dogs',
+            name:"Собаки",
+            url:dogs
+        },{
+            type:'rats',
+            name:"Грызуны",
+            url:rats
+        },{
+            type:'fish',
+            name:"Рыбы",
+            url:fish
+        },{
+            type:'bird',
+            name:"Птицы",
+            url:bird
+        },
+
+    ]
     return (
         <div className='wrapper-filter-bar'>
             <div className='filter-bar'>
-                <div className='cats'><div className='name'>Кошки</div><div className="svg"><img src={cat}/></div></div>
-                <div className='dogs'><div className='name'>Собаки</div><div className="svg"><img src={dog}/></div></div>
-                <div className='rats'><div className='name'>Грызуны</div><div className="svg"><img src={rat}/></div></div>
-                <div className='fish'><div className='name'>Рыбки</div><div className="svg"><img src={fish}/></div></div>
-                <div className='ferrets'><div className='name'>Птицы</div><div className="svg"><img src={bird}/></div></div>
+                {Categories.map(item=><div onClick={()=>setactive(item.type)} className={classNames('cats',{'active':active==item.type})}><div className='name'>{item.name}</div><div className="svg"><img src={item.url}/></div></div>)}
             </div>
         </div>
     );
