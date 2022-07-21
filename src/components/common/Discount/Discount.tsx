b2f350ecf7aea42df2d49fdf0ebaffe6634dcd5e
Egor <egorlavrinovich@gmail.com>
2022-07-21 11:57:31 +0300

    Настроен таймер хз как но работает

--- src/components/common/Discount/Discount.tsx
+++ src/components/common/Discount/Discount.tsx
@@ -1,27 +1,32 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import {useAppSelector} from "../../../hooks/Reduxhooks";
 // @ts-ignore
 import images from './Allimports'
 import {ArrowLeft,ArrowRight} from 'react-bootstrap-icons'
+import {clearInterval} from "timers";
 const Discount = React.memo(() => {
     const [activeblock,setactiveblock] = useState(false)
-    const [activeimage,setactiveimage] = useState(1)
+    const [activeimage,setactiveimage] = useState(0)
+    const ff = useRef(0)
+    // @ts-ignore
+    ff.current = activeimage
     const Available = useAppSelector((state)=>state.categories.choosedcategory)
     useEffect(()=>{if(Available==='discount')setactiveblock(true)
     else setactiveblock(false)},[Available])
     function SpinBlock(perem:string){
         switch (perem){
-            case 'right':setactiveimage((x)=>x+1); break
-            case 'left':setactiveimage((x)=>x-1);break
+            case 'right':setactiveimage(()=>ff.current+1);clearTimeout(Timeout.current); break
+            case 'left':setactiveimage(()=>ff.current-1);clearTimeout(Timeout.current);break
         }
     }
-    setTimeout(()=>{
-        setactiveimage((x)=>x+1)
-    },6000)
+    // @ts-ignore
+    const Timeout = useRef()
     useEffect(()=>{
         if(activeimage>6) setactiveimage(0)
         if(activeimage<0) setactiveimage(6)
-
+        // @ts-ignore
+        Timeout.current=setTimeout(()=>{setactiveimage(()=>ff.current+1)},2000)
+        console.log(Timeout.current)
     },[activeimage])
     return (
         <div className='discount-wrapper'>
