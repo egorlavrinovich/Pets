4233d6eb83ee314057b626d4f21ad9aff357fb38
Egorlaptop <egorlavrinovich@gmail.com>
2022-07-24 11:57:56 +0300

    SLIDER BOBAVLEN EPTEL

--- src/components/UI/Input/InputRange/InputRange.tsx
+++ src/components/UI/Input/InputRange/InputRange.tsx
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
-import Slider from "rc-slider";
-import FormRange from "react-bootstrap/FormRange";
-
+import React, {useState,ReactNode} from 'react';
+import 'rc-slider/assets/index.css';
+// @ts-ignore
+import Slider from 'rc-slider'
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 const InputRange = () => {
-    const [value,setvalue] = useState('0')
-    const onHandleChange = (e:React.ChangeEvent<HTMLInputElement>) => setvalue(e.target.value)
+    const [value,setvalue] = useState('170')
+    const [state,setstate] = useState<string>('0')
+    const [range,setrange] = useState([0,100])
+    const onHandleChange = (e:React.ChangeEvent<HTMLInputElement>) => setvalue(e.target.value);
+    const onChangeRange = (e:any)=> setrange(e)
     return (
         <div className='input-range-wrapper'>
-            <div>{value}</div>
+            <Range
+                marks={{
+                    100: `$ 100`,
+                    500: `$ 500`
+                }}
+                min={100}
+                max={500}
+                defaultValue={[200, 300]}
+                // @ts-ignore
+
+                tipFormatter={value => `$ ${value}`}
+                tipProps={{
+                    placement: "top",
+                    visible: true
+                }}
+            />
+            <div>{`Цена до: ${value} руб`}</div>
             <input value={value} type='range' min={0} max={170} onChange={(e)=>onHandleChange(e)} />
-
-
         </div>
     );
 };
