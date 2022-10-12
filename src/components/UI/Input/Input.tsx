1b5f33734e10816688d10fd54c2f8a13e7c13dcb
Egorlaptop <egorlavrinovich@gmail.com>
2022-10-12 15:00:57 +0300

    Add price input

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,18 +1,33 @@
-import React, {FC, FocusEventHandler} from 'react';
+import React, { FC, FocusEventHandler } from "react";
 
-interface IInput{
-    type:string;
-    placeholder:string;
-    value?:string|any;
-    onChange?:React.ChangeEventHandler | undefined;
-    name?:string;
-    onBlur?:React.EventHandler<any>
+interface IInput {
+  type: string;
+  placeholder: string;
+  value?: string | any;
+  onChange?: React.ChangeEventHandler | undefined;
+  name?: string;
+  onBlur?: React.EventHandler<any>;
 }
 
-const Input:FC<IInput> = ({onBlur,type,name,placeholder,value,onChange}) => {
-    return (
-        <input onBlur={onBlur} value={value} type={type} placeholder={placeholder} onChange={onChange} name={name}/>
-    );
+const Input: FC<IInput> = ({
+  onBlur,
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  // @ts-ignore
+  return (
+    <input
+      onBlur={onBlur}
+      value={value}
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      name={name}
+    />
+  );
 };
 
 export default Input;
--- src/components/UI/Input/InputRange/InputRange.tsx
+++ src/components/UI/Input/InputRange/InputRange.tsx
@@ -1,44 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import 'rc-slider/assets/index.css';
+import React, { useState, useEffect, ChangeEvent } from "react";
+import "rc-slider/assets/index.css";
 // @ts-ignore
-import Slider from 'rc-slider'
-import {useDispatch} from "react-redux";
-import {addRange} from "../../../../Redux/FilterSlice";
-import {UseChangeInput} from "../../../../hooks/UseChangeInput";
+import Slider from "rc-slider";
+import { useDispatch } from "react-redux";
+import { addRange } from "../../../../Redux/FilterSlice";
+import { UseChangeInput } from "../../../../hooks/UseChangeInput";
 import Input from "../Input";
 const { createSliderWithTooltip } = Slider;
 const Range = createSliderWithTooltip(Slider.Range);
 const InputRange = React.memo(() => {
-    const [state,setstate] = useState<any>([0,500]) // выбранная цена пользователем
-    const [range,setrange] = useState([0,500]) // Диапазон цен
-    const [active,setactiv] = useState<boolean>(false) // маркер наблюдение за ценой пользователя
-    const dispatch=useDispatch()
-    useEffect(()=>{
-        // @ts-ignore
-        dispatch(addRange(state)) // добавляем фильтр для цены
-    },[active])
-    const [example,setexample] = useState([0,100])
-    console.log(example)
-    return (
-        <div className='input-range-wrapper'>
-            {/*// @ts-ignore*/}
-            <Input type='text' value={example[0]} onChange={(event)=>setexample([+event.target.value,...example.slice(1)])} placeholder=''/>
-            {/*// @ts-ignore*/}
-            <Input type='text' value={example[1]} onChange={(event)=>setexample([...example.splice(2,1),+event.target.value,])} placeholder=''/> //дописать функционал
-            <Range
-                value={state}
-                marks={{
-                    0: `0 руб`,
-                    500: `500 руб`
-                }}
-                min={range[0]}
-                max={range[1]}
-                defaultValue={[0, 500]}
-                onChange={(e:[])=>setstate(e)}
-                onBlur={()=>setactiv(!active)}
-            />
-        </div>
-    );
+  const [state, setstate] = useState<any>([0, 500]); // выбранная цена пользователем
+  const [range, setrange] = useState([0, 500]); // Диапазон цен
+  const [active, setactiv] = useState<boolean>(false); // маркер наблюдение за ценой пользователя
+  const [rangePriceInput, setRangePriceInput] = useState<any>([0, 100]); // фильтр цен
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(addRange(state)); // добавляем фильтр для цены
+  }, [active]);
+  console.log(rangePriceInput);
+  return (
+    <div className="input-range-wrapper">
+      <Input
+        type="text"
+        value={rangePriceInput[0]}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setRangePriceInput([+e.target.value, rangePriceInput[1]])
+        }
+        placeholder=""
+      />
+      <Input
+        type="text"
+        value={rangePriceInput[1]}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setRangePriceInput([rangePriceInput[0], +e.target.value])
+        }
+        placeholder=""
+      />{" "}
+      <Range
+        value={state}
+        marks={{
+          0: `0 руб`,
+          500: `500 руб`,
+        }}
+        min={range[0]}
+        max={range[1]}
+        defaultValue={[0, 500]}
+        onChange={(e: []) => setstate(e)}
+        onBlur={() => setactiv(!active)}
+      />
+    </div>
+  );
 });
 
 export default InputRange;
